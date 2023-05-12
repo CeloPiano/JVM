f1d34e1556ddc512c818917c894460d5c0a4e6b5
Marcelo Piano <marcelopianopatuscosantiago@gmaill.com>
2023-05-12 08:44:25 -0300

    fix: commented some structs and added tag_to_class_enum

--- leitor_exibidor.h
+++ leitor_exibidor.h
@@ -44,73 +44,75 @@ static u8 u8Read();
 
 
 
-// Aqui temos os constant value attributes 
+// Aqui temos os constant value attributes para a UNION attribute_info_union
 
-/// @brief Exception Table Info
-typedef struct exception_table{
-    u2 start_pc;
-    u2 end_pc;
-    u2 handler_pc;
-    u2 catch_type;
-} exception_table;
+// /// @brief Exception Table Info
+// typedef struct exception_table{
+//     u2 start_pc;
+//     u2 end_pc;
+//     u2 handler_pc;
+//     u2 catch_type;
+// } exception_table;
 
 
-// se for abstract ou native nao tem codigo... se não, temos que ter apenas um atributo de código
-/// @brief Struct Code Attribute info
-typedef struct Code_atribute{
-    u2 max_stack;
-    u2 max_locals;
-    u2 code_lenght;
-    u1 *code;
-    u2 exception_table_length;
-    *exception_table; 
-    u2 attribute_count;
-    attribute_info *attributes
+// // se for abstract ou native nao tem codigo... se não, temos que ter apenas um atributo de código
+// /// @brief Struct Code Attribute info
+// typedef struct Code_atribute{
+//     u2 max_stack;
+//     u2 max_locals;
+//     u2 code_lenght;
+//     u1 *code;
+//     u2 exception_table_length;
+//     *exception_table; 
+//     u2 attribute_count;
+//     attribute_info *attributes
     
-} ConstantValue_attribute;
+// } ConstantValue_attribute;
 
-// isso é necessário?
-/// @brief Struct Constant Value Attribute info
-typedef struct ConstantValue_attribute{
-    u2 constantvalue_index;
-} ConstantValue_attribute;
+// // isso é necessário?
+// /// @brief Struct Constant Value Attribute info
+// typedef struct ConstantValue_attribute{
+//     u2 constantvalue_index;
+// } ConstantValue_attribute;
 
 
-/// @brief Struct Attribute info
+// /// @brief Struct Attribute info
+// typedef struct attribute_info{
+//     u2 attribute_name_index;
+//     u4 attribute_lenght;
 
-typedef struct attribute_info{
-    u2 attribute_name_index;
-    u4 attribute_lenght;
-    // aqui temos as diferencas entre os atributos
-    union {
+//     // aqui temos as diferencas entre os atributos
+//     union {
         
 
-    } attribute_info_union;
-}
+//     } attribute_info_union;
+// };
 
-/// @brief Struct do Method
-typedef struct field_info{
-    u2 acess_flags; 
-    u2 name_index;          //aqui nesse caso pode ser <init>, <clinit> ou um nome valido para o metodo
-    u2 descriptor_index;            
-    u2 attributes_count;
-    attribute_info attributes**;  //aqui acessamos o atribute info utilizando o count para acessar uma estrutura de atribute info
+// /// @brief Struct do Method
+// typedef struct method_info{
+//     u2 acess_flags; 
+//     u2 name_index;          //aqui nesse caso pode ser <init>, <clinit> ou um nome valido para o metodo
+//     u2 descriptor_index;            
+//     u2 attributes_count;
+//     attribute_info **attributes;  //aqui acessamos o atribute info utilizando o count para acessar uma estrutura de atribute info
+
+// };
 
 /// @brief Struct do Field
-
-typedef struct field_info{
-    u2 acess_flags; 
-    u2 name_index;
-    u2 descriptor_index;
-    u2 attributes_count;
-    attribute_info **attributes;  //aqui acessamos o atribute info utilizando o count
-}
+// typedef struct field_info{
+//     u2 acess_flags; 
+//     u2 name_index;
+//     u2 descriptor_index;
+//     u2 attributes_count;
+//     attribute_info **attributes;  //aqui acessamos o atribute info utilizando o count
+// };
 
 /// @brief Struct cp_info
 typedef struct cp_info{
     u1 tag;
 
-    union constant_type{
+    union constant_type_union{
+
         struct{
             u2 name_index;
         } Class_info;
@@ -138,7 +140,7 @@ typedef struct cp_info{
         struct{
             u2 length;          //indica o número de bytes no array bytes (embaixo) pode ser 1, 2 ou 3 bytes
             u1 *bytes;          // quando formos alocar os bytes levamos em conta o numero de byts aqui é um array
-            u1 *bytes = (u1 *) malloc(lenght * sizeof(u1));     //aqui criamos um espaço de memória do tamanho do problema. (u1 tem 1 bit)
+            // u1 *bytes = (u1 *) malloc(lenght * sizeof(u1));     //aqui criamos um espaço de memória do tamanho do problema. (u1 tem 1 bit)
         } Utf8;
         
         struct{
@@ -191,3 +193,23 @@ typedef struct Classfile
 
 } Classfile;
 
+
+// Enum para [POOL DE CONSTANTES TIPOS VÁLIDOS DE TAGS]
+
+enum tag_to_class
+{
+    CONSTANT_Class_info = 7,
+    CONSTANT_Fieldref_info = 9,
+    CONSTANT_Methodref_info = 10,
+    CONSTANT_InterfaceMethodref_info = 11,
+    CONSTANT_String_info = 8,
+    CONSTANT_Integer_info = 3,
+    CONSTANT_Float_info = 4,
+    CONSTANT_Long_info = 5,
+    CONSTANT_Double_info = 6,
+    CONSTANT_NameAndType_info = 12,
+    CONSTANT_Utf8_info = 1,
+    CONSTANT_MethodHandle_info = 15,
+    CONSTANT_MethodType_info = 16,
+    CONSTANT_InvokeDynamic_info = 18
+};
